a81186d2a907bf68c03c4e6d680c227c49993b33
rex <chaodongqu@163.com>
2017-12-11 22:38:54 +0800

    test hase

---
+++ src/main/java/com/rex/test_h/TestHApplication.java
@@ -0,0 +1,12 @@
+package com.rex.test_h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rex/test_h/TestHApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rex.test_h;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestHApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
